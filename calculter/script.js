4639970e78918982269aeeb0142675cac51c3c78
Hamaddogar <hamadali77115033@gmail.com>
2020-04-23 08:03:18 +0500

    calculater

---
+++ calculter/script.js
@@ -0,0 +1,45 @@
+//fucntion decalration 
+  // function Expression 
+   // hositng // 
+   //class Exprestion  
+   // Contructer
+    // super()
+
+    //this
+
+
+
+
+
+    // MyFunction()
+
+
+
+//Decaration
+// function MyFunction()
+// {
+ //
+//   var  objectname ={
+//          frindname:"Daniyal Brother",
+//           professtional:"IT",
+//            aboutme:function(){
+//                   return  this.frindname
+
+//            }
+
+//   }
+
+// console.log(objectname.aboutme())
+ 
+// }
+
+
+
+ //fuicntion Expresstion
+  var funtionExpre=function(){
+
+  console.log("this is fucntion Expression")
+
+
+      }
+      funtionExpre()
